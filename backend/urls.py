4e6421001d508b8bbe632bf8d940ec8b939fd90d
Nixon <nixon.emoony@gmail.com>
2019-03-01 20:22:45 +0000

    AGPL

--- backend/urls.py
+++ backend/urls.py
@@ -16,6 +16,22 @@ Including another URLconf
 import os
 
 from django.contrib import admin
+# Copyright (C) 2019 Nixon Enraght-Moony
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 from django.urls import path
 from django.views.generic import TemplateView
 from django.conf.urls import url
